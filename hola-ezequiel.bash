5d5e4ff64f474e2491a464c7a0cd67c9865ecd36
Ezequiel Crosa <ezequiel@kudosestudio.com>
2024-07-04 10:36:40 -0300

    main

--- hola-ezequiel.bash
+++ hola-ezequiel.bash
@@ -6,15 +6,17 @@ findandcd() {
         return 1
     fi
 
+    # Obtener solo el dominio de la URL
     URL=$1
+    DOMAIN=$(echo "$URL" | sed -E 's~https?://~~' | awk -F[/:] '{print $1}')
 
     # Primero buscar en /etc/nginx/sites-enabled
-    FILES=$(grep -rl "$URL" /etc/nginx/sites-enabled/*)
+    FILES=$(grep -rl "$DOMAIN" /etc/nginx/sites-enabled/*)
     if [ -z "$FILES" ]; then
         # Si no encuentra resultados, buscar en /etc/nginx
-        FILES=$(grep -rl "$URL" /etc/nginx/)
+        FILES=$(grep -rl "$DOMAIN" /etc/nginx/)
         if [ -z "$FILES" ]; then
-            echo "No se encontraron archivos que contengan \"$URL\" en /etc/nginx/sites-enabled/ ni en /etc/nginx/"
+            echo "No se encontraron archivos que contengan \"$DOMAIN\" en /etc/nginx/sites-enabled/ ni en /etc/nginx/"
             return 1
         fi
     fi
@@ -26,10 +28,10 @@ findandcd() {
         echo "No se encontró la ruta del MAGE_ROOT en $FIRST_FILE"
         return 1
     fi
-    DOMAIN=$(grep -oP "server_name \K[^;]+" "$FIRST_FILE" | head -n 1)
+    DOMAIN_FOUND=$(grep -oP "server_name \K[^;]+" "$FIRST_FILE" | head -n 1)
 
     echo "Archivo encontrado: $FIRST_FILE"
-    echo "Dominio encontrado: $DOMAIN"
+    echo "Dominio encontrado: $DOMAIN_FOUND"
 
     cd "$MAGE_ROOT" || { echo "No se pudo cambiar al directorio $MAGE_ROOT"; return 1; }
 }
@@ -37,4 +39,3 @@ findandcd() {
 alias holafindandcd='findandcd'
 
 export VIMINIT=':set mouse-=a'
-
