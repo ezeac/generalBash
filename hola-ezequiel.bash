01da2db65f5bd64fe88b95d75f5f231a51391f96
Ezequiel Crosa <ezequiel@kudosestudio.com>
2024-08-01 14:18:28 -0300

    main

--- hola-ezequiel.bash
+++ hola-ezequiel.bash
@@ -10,35 +10,54 @@ findandcd() {
     URL=$1
     DOMAIN=$(echo "$URL" | sed -E 's~https?://~~' | awk -F[/:] '{print $1}')
 
-    # Primero buscar en /etc/nginx/sites-enabled
-    FILES=$(grep -rl "$DOMAIN" /etc/nginx/sites-enabled/*)
+    # Función para buscar en un directorio específico
+    search_in_directory() {
+        local DIRECTORY=$1
+        grep -rl "$DOMAIN" "$DIRECTORY"
+    }
+
+    # Buscar en /etc/nginx/sites-enabled
+    FILES=$(search_in_directory /etc/nginx/sites-enabled)
     if [ -z "$FILES" ]; then
-        # Si no encuentra resultados, buscar en /etc/nginx
-        FILES=$(grep -rl "$DOMAIN" /etc/nginx/)
+        # Si no encuentra resultados, buscar en /etc/nginx/conf.d
+        FILES=$(search_in_directory /etc/nginx/conf.d)
         if [ -z "$FILES" ]; then
-            echo "No se encontraron archivos que contengan \"$DOMAIN\" en /etc/nginx/sites-enabled/ ni en /etc/nginx/"
-            return 1
+            # Si no encuentra resultados, buscar en /etc/nginx
+            FILES=$(search_in_directory /etc/nginx)
+            if [ -z "$FILES" ]; then
+                echo "No se encontraron archivos que contengan \"$DOMAIN\" en /etc/nginx/sites-enabled/, /etc/nginx/conf.d/ ni en /etc/nginx/"
+                return 1
+            fi
         fi
     fi
 
     # Obtener los primeros 10 archivos
     FILES=$(echo "$FILES" | head -n 10)
 
+    RESULTS=()
+
     for FILE in $FILES; do
         MAGE_ROOT=$(grep -oP "set \\\$MAGE_ROOT \K[^;]+" "$FILE")
         if [ -n "$MAGE_ROOT" ]; then
             DOMAIN_FOUND=$(grep -oP "server_name \K[^;]+" "$FILE" | head -n 1)
-            echo "Archivo encontrado: $FILE"
-            echo "Dominio encontrado: $DOMAIN_FOUND"
-            cd "$MAGE_ROOT" || { echo "No se pudo cambiar al directorio $MAGE_ROOT"; return 1; }
-            return 0
+            RESULTS+=("Archivo encontrado: $FILE")
+            RESULTS+=("Dominio encontrado: $DOMAIN_FOUND")
         fi
     done
 
-    echo "No se encontró la ruta del MAGE_ROOT en los primeros 10 archivos"
-    return 1
+    if [ ${#RESULTS[@]} -eq 0 ]; then
+        echo "No se encontró la ruta del MAGE_ROOT en los primeros 10 archivos"
+        return 1
+    else
+        for RESULT in "${RESULTS[@]}"; do
+            echo "$RESULT"
+        done
+        cd "$MAGE_ROOT" || { echo "No se pudo cambiar al directorio $MAGE_ROOT"; return 1; }
+        return 0
+    fi
 }
 
 alias holafindandcd='findandcd'
 
 export VIMINIT=':set mouse-=a'
+
